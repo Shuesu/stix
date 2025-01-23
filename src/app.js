d7a10b5478d081438046b4d4515c3ca9320eb2af
Atabayev Ali <sptmbrw@gmail.com>
2025-01-23 16:38:20 +0500

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const App = () => {
+	return (
+		<div>
+			<div>
+				<h1>Заголовок приложения</h1>
+				<div>
+					{/* Добавьте ваш контент здесь */}
+				</div>
+			</div>
+		</div>
+	);
+};
